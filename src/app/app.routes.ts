e776a5854b8acf60aadd69252349cfc2a9d7ca53
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-02-05 18:56:19 +0530

    feat: update routing to include main layout and not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,34 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
-import { HomeComponent } from './features/home/home.component';
 import { AuthCallbackComponent } from './features/auth/auth-callback/auth-callback.component';
 import { AuthGuard } from './core/guards/auth-guard/auth.guard';
 import { GuestGuard } from './core/guards/guest-guard/guest.guard';
+import { NotFoundComponent } from './features/error-pages/not-found/not-found.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./features/home/home.component').then((m) => m.HomeComponent),
+        canActivate: [AuthGuard],
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
   {
     path: 'auth-callback',
     component: AuthCallbackComponent,
     canActivate: [GuestGuard],
   },
   { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundComponent },
 ];
