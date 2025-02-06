3a2351311141510f1876a1a6e51f3cc203981101
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-02-06 15:38:39 +0530

    doc: add comments

--- src/app/state/user/user.effects.ts
+++ src/app/state/user/user.effects.ts
@@ -4,6 +4,11 @@ import { setUser, clearUser } from './user.actions';
 import { map, mergeMap } from 'rxjs/operators';
 import { Auth0Service } from '../../core/services/auth0/auth0.service';
 
+/**
+ * =========================================
+ * ⚠️ NOT IN USE - Kept for reference only ⚠️
+ * =========================================
+ */
 @Injectable()
 export class UserEffects {
   constructor(
@@ -11,15 +16,17 @@ export class UserEffects {
     private auth0Service: Auth0Service
   ) { }
 
+  // Effect to load the user data when the 'Load User' action is dispatched
   loadUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType('[App] Load User'),
       mergeMap(() =>
         this.auth0Service.getClient().getUser().then(user => {
           if (user) {
+            // If a user is found, dispatch the 'setUser' action to update the state with the user data
             return setUser({ user });
           } else {
-            // handle the case where user is undefined, e.g., return an empty action or log an error
+            // Handle the case where user is undefined.
             return { type: '[User] No User Found' };
           }
         })
@@ -27,11 +34,14 @@ export class UserEffects {
     )
   );
 
+  // Effect to log out the user when the 'Logout User' action is dispatched
+  // Way to call: this.store.dispatch({ type: '[App] Load User'});
   logoutUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType('[App] Logout User'),
       map(() => {
         this.auth0Service.logout();
+        // Dispatching the 'clearUser' action to clear the user data from the state
         return clearUser();
       })
     )
--- src/app/state/user/user.selectors.ts
+++ src/app/state/user/user.selectors.ts
@@ -1,8 +1,13 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { UserState } from './user.reducer';
 
+// createFeatureSelector creates a selector that selects a slice of the state from the global store.
+// 'user' is the feature key that corresponds to a part of the state managed by the user reducer.
 export const selectUserState = createFeatureSelector<UserState>('user');
 
+// createSelector is used to create a selector that derives part of the state from the 'user' feature.
+// It takes the 'selectUserState' selector (which selects the entire user slice of state),
+// and then extracts the 'user' property from the 'UserState' to return it.
 export const selectUser = createSelector(
   selectUserState,
   (state: UserState) => state.user
